cefc4203d345e2380c27b0d495f9f7de0223f62b
Marcus M. Darden <ontoral@gmail.com>
2014-04-01 02:26:11 -0400

    Add LC-3 Bubble Sort.

---
+++ lc3/numbers.asm
@@ -0,0 +1,18 @@
+; Set up a list of numbers to be sorted for the bubble sort assignment
+
+        .ORIG       x3020
+        .FILL       #10
+        .FILL       #7
+        .FILL       #4
+        .FILL       #1
+        .FILL       #5
+        .FILL       #8
+        .FILL       #2
+        .FILL       #11
+        .FILL       #20
+        .FILL       #3
+        .FILL       #6
+        .FILL       #10
+        .FILL       #9
+        .END
+
