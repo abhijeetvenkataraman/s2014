114cb51cf66cf5d1329da0986ecae1ef1cd81339
Marcus M. Darden <ontoral@gmail.com>
2014-04-23 01:29:26 -0400

    Standardize capitalization.

--- lc3/factr.asm
+++ lc3/factr.asm
@@ -1,8 +1,8 @@
             .ORIG   x3000
 
-            LD      R6, BASE        ; Initialize Stack (R6)
-            NOT     R6, R6          ; ...negate BASE
-            ADD     R6, R6, #1      ; R6 = -BASE
+            LD      R6, Base        ; Initialize Stack (R6)
+            NOT     R6, R6          ; ...negate Base
+            ADD     R6, R6, #1      ; R6 = -Base
             AND     R0, R0, #0
             ADD     R0, R0, #4
             JSR     FACTR           ; Call 4!
@@ -25,11 +25,11 @@ FACTR       ST      R0, SaveFact    ; Save n
             ADD     R5, R5, #0      ; Check for underflow
             BRp     Cleanup         ; Wrap up if stack is empty
             ST      R0, SaveLink    ; Save return link
-            ST      R1, SAVE_R1     ; Protect R1
+            ST      R1, SaveR1      ; Preserve R1
             JSR     POP             ; Pop n from stack
             LD      R1, SaveFact    ; Restore (n - 1)! to R1
             JSR     MUL             ; R0 = n * (n - 1)!
-            LD      R1, SAVE_R1     ; Retrieve R1
+            LD      R1, SaveR1      ; Retrieve R1
             LD      R7, SaveLink    ; Restore return link to R7
             BR      Done            ; All done!
 Cleanup     LD      R0, SaveFact    ; Stack underflow: R0 = (n - 1)!
@@ -37,40 +37,41 @@ Done        RET
 
 SaveLink    .FILL   x0
 SaveFact    .FILL   x0
-SAVE_R1     .FILL   x0
+SaveR1      .FILL   x0
 
+; Fake multiplication subroutine
 MUL         ADD     R0, R0, R1
             RET
 
-POP         ST      R2, SAVE2
-            ST      R1, SAVE1
-            LD      R1, BASE
+POP         ST      R2, Save2
+            ST      R1, Save1
+            LD      R1, Base
             ADD     R1, R1, #-1
             ADD     R2, R6, R1
-            BRz     FAILURE
+            BRz     Failure
             LDR     R0, R6, #0
             ADD     R6, R6, #1
-            BR      SUCCESS
-PUSH        ST      R2, SAVE2
-            ST      R1, SAVE1
-            LD      R1, MAX
+            BR      Success
+PUSH        ST      R2, Save2
+            ST      R1, Save1
+            LD      R1, Max
             ADD     R2, R6, R1
-            BRz     FAILURE
+            BRz     Failure
             ADD     R6, R6, #-1
             STR     R0, R6, #0
-SUCCESS     LD      R1, SAVE1
-            LD      R2, SAVE2
+Success     LD      R1, Save1
+            LD      R2, Save2
             AND     R5, R5, #0
             RET
-FAILURE     LD      R1, SAVE1
-            LD      R2, SAVE2
+Failure     LD      R1, Save1
+            LD      R2, Save2
             AND     R5, R5, #0
             ADD     R5, R5, #1
             RET
 
-BASE        .FILL   xC001
-MAX         .FILL   xC00A
-SAVE1       .FILL   x0
-SAVE2       .FILL   x0
+Base        .FILL   xC001
+Max         .FILL   xC00A
+Save1       .FILL   x0
+Save2       .FILL   x0
 
             .END
